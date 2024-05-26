b8fff79b8a02b5d5c44ca01eb77e4152c95e1946
juamp <juamp@DESKTOP-BAAFIF4>
2024-05-26 20:18:44 +0200

    clase leer y crear pdf de inscripcion faltan metodos crear y leer actas.

---
+++ src/main/java/PdfCrear.java
@@ -0,0 +1,194 @@
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+import com.spire.pdf.PdfDocument;
+import com.spire.pdf.PdfPageBase;
+import com.spire.pdf.fields.PdfTextBoxField;
+import com.spire.pdf.graphics.PdfBrush;
+import com.spire.pdf.graphics.PdfFont;
+import com.spire.pdf.graphics.PdfFontFamily;
+import com.spire.pdf.graphics.PdfRGBColor;
+import com.spire.pdf.graphics.PdfSolidBrush;
+
+
+
+public class PdfCrear {
+	
+	
+	
+	
+	public  PdfCrear crearpdfFormulario(String nombreTorneo,int cantidadJugadores) {
+		PdfDocument doc = new PdfDocument();
+
+	    PdfPageBase page = doc.getPages().add();
+	    
+	    // Crear fuentes y pinceles para el texto
+	    PdfFont font = new PdfFont(PdfFontFamily.Helvetica, 8f);
+	    PdfFont fontT = new PdfFont(PdfFontFamily.Times_Roman, 16f);
+	    PdfBrush brush = new PdfSolidBrush(new PdfRGBColor(Color.black));
+		
+		
+	    
+	    // Definir la posición inicial para los campos del formulario
+	    
+	    float y = 50;
+	    float width = 100;
+	    float height = 15;
+	 
+	    
+	    page.getCanvas().drawString("Inscripción Equipo", fontT, brush, new Point2D.Float(150,25));
+	    page.getCanvas().drawString(nombreTorneo, fontT, brush, new Point2D.Float(150,50));
+	    
+	    // Añadir campo para el nombre
+	    	
+	    y+=25;	
+	    try {
+	    	page.getCanvas().drawString("Nombre del equipo:", font, brush, new Point2D.Float(100, y));
+	    	
+	    	Rectangle2D.Float tbxBounds = new Rectangle2D.Float(250, y,200, height);
+	        
+			PdfTextBoxField equipoNombre = new PdfTextBoxField(page, "nombreEquipo");
+			equipoNombre.setBounds(tbxBounds);
+			equipoNombre.setText("Aqui Nombre de equipo");
+			equipoNombre.setFont(font);
+			doc.getForm().getFields().add(equipoNombre);
+			y += 25;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	   	
+	    try {
+	    	page.getCanvas().drawString("Correo electronico:", font, brush, new Point2D.Float(100, y));
+	    	
+	    	Rectangle2D.Float tbxBounds = new Rectangle2D.Float(250, y,200, height);
+	        
+			PdfTextBoxField equipoNombre = new PdfTextBoxField(page, "correo Electronico");
+			equipoNombre.setBounds(tbxBounds);
+			equipoNombre.setText("correo electronico");
+			equipoNombre.setFont(font);
+			doc.getForm().getFields().add(equipoNombre);
+			y += 25;
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	 
+	    String titulo="";
+	    //crear bucle para repetir estructura en base a numero de jugadores
+	    
+	    for(int i=0;i<=cantidadJugadores;i++) {
+	    	if(i==7) {
+	    		y=50;
+	    	page =	 doc.getPages().add();
+	    	}
+	     if(i==0) {
+	    	 titulo="Jugador/Responsable";
+	     }else {
+	    	 if(i==1) {
+	    		 titulo="Jugador/Arbitro";
+	    	 }else {
+	    		 titulo="Jugador";
+	    	 }
+	     }
+	     
+	    //titulo para Entrenador
+	    page.getCanvas().drawString(titulo, fontT, brush, new Point2D.Float(50,y));
+	    y+=20;
+	    // etiquetas campos
+	    page.getCanvas().drawString("Nombre", font, brush, new Point2D.Float(50,y));
+	    page.getCanvas().drawString("Apellido 1", font, brush, new Point2D.Float(200,y));
+	    page.getCanvas().drawString("Apellido 2", font, brush, new Point2D.Float(400,y));
+	    y+=10;
+		try {
+			PdfTextBoxField nombreCampo;
+			nombreCampo = new PdfTextBoxField(page, "Nombre");
+			 nombreCampo.setBounds(new Rectangle2D.Float(50, y, width, height));
+			 	nombreCampo.setText("aqui el nombre");
+		        nombreCampo.setFont(font);
+		        doc.getForm().getFields().add(nombreCampo);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	   
+	    
+	    // Añadir campo para los apellidos
+	   
+	    // Aumentar la posición en Y para el siguiente campo
+	    
+		try {
+			PdfTextBoxField apellido1Campo;
+			apellido1Campo = new PdfTextBoxField(page, "Apellido1");
+			apellido1Campo.setBounds(new Rectangle2D.Float(200, y, width, height));
+			apellido1Campo.setText("aqui el apellido");
+			apellido1Campo.setFont(font);
+	        doc.getForm().getFields().add(apellido1Campo);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+			// Aumentar la posición en Y para el siguiente campo
+	    
+		try {
+			PdfTextBoxField apellido2Campo;
+			apellido2Campo = new PdfTextBoxField(page, "Apellido2");
+			apellido2Campo.setBounds(new Rectangle2D.Float(400, y, width, height));
+			apellido2Campo.setText("aqui el 2º apellido");
+			apellido2Campo.setFont(font);
+	        doc.getForm().getFields().add(apellido2Campo);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	    
+		
+		
+		
+	    // Añadir campo para el DNI
+	    y+=15; 
+	    
+	 
+		try {
+			page.getCanvas().drawString("DNI", font, brush, new Point2D.Float(50,y));
+			y +=10;
+			PdfTextBoxField dniCampo;
+			dniCampo = new PdfTextBoxField(page, "Dni");
+			
+			dniCampo.setBounds(new Rectangle2D.Float(50, y, width, height));
+			dniCampo.setText("aqui DNI");
+	        dniCampo.setFont(font);
+	        doc.getForm().getFields().add(dniCampo);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			y-=10;
+			page.getCanvas().drawString("Fecha de nacimiento dd/MM/YY", font, brush, new Point2D.Float(200,y));
+			y +=10;
+			PdfTextBoxField fechaCampo;
+			fechaCampo = new PdfTextBoxField(page, "fechanNacimiento");
+			
+			fechaCampo.setBounds(new Rectangle2D.Float(200, y, width, height));
+			fechaCampo.setText("aqui fecha");
+	        fechaCampo.setFont(font);
+	        doc.getForm().getFields().add(fechaCampo);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	y+=25;
+	    }
+	    
+	    // Guardar el documento PDF
+	    doc.saveToFile("FormularioUsuario5.pdf");
+	    doc.close();
+		return null;
+	}
+}
---
+++ src/main/java/PdfLeer.java
@@ -0,0 +1,104 @@
+import com.spire.pdf.PdfDocument;
+import com.spire.pdf.fields.PdfField;
+import com.spire.pdf.widget.PdfFormWidget;
+import com.spire.pdf.widget.PdfTextBoxFieldWidget;
+
+
+
+public class PdfLeer {
+	
+	
+	
+public  PdfLeer leerPdf() {
+		
+		// Cargar el documento PDF
+        PdfDocument pdf = new PdfDocument();
+        pdf.loadFromFile("FormularioUsuario5relleno.pdf");
+
+        // Obtener el widget del formulario
+        PdfFormWidget formWidget = (PdfFormWidget) pdf.getForm();
+
+        StringBuilder sb = new StringBuilder();
+        String texto1="";
+        // Recorrer la colección de widgets de campos y extraer los valores
+        for (int i = 0; i < 2; i++) {
+        	
+        	if(i==0) {
+        		texto1="el nombre de equipo es ";
+        	}else {
+        		texto1="el correo de contacto es ";
+        	}
+            PdfField field = (PdfField) formWidget.getFieldsWidget().getList().get(i);
+
+            if (field instanceof PdfTextBoxFieldWidget) {
+                PdfTextBoxFieldWidget textBoxField = (PdfTextBoxFieldWidget) field;
+                String texto = textBoxField.getText();
+                System.out.println(texto1 + texto + "\r\n");
+                sb.append("El texto en el campo de texto es: " + texto + "\r\n");
+            }
+            // También puedes manejar otros tipos de campos aquí (listas desplegables, etc.).
+
+            // ...
+        }
+        for (int i = 2; i < formWidget.getFieldsWidget().getCount(); i++) {
+            
+        		
+            for(int j=0;j<5;j++) {
+            	switch (j) {
+				case 0: {
+					texto1="nombre de jugador= ";
+					break;
+					}
+				case 1: {
+					texto1="apellido 1 de jugador= ";
+					break;
+					}
+				case 2: {
+					texto1="apellido 2 de jugador= ";
+					break;
+					}
+				case 3: {
+					texto1="dni de jugador= ";
+					break;
+					}
+				case 4: {
+					texto1="fecha de nacimiento de jugador= ";
+					break;
+					}
+				}
+            	PdfField field = (PdfField) formWidget.getFieldsWidget().getList().get(i);
+            	 if (field instanceof PdfTextBoxFieldWidget) {
+                     PdfTextBoxFieldWidget textBoxField = (PdfTextBoxFieldWidget) field;
+                     String texto = textBoxField.getText();
+                     System.out.println(texto1 + texto + "\r\n");
+                     sb.append("El texto en el campo de texto es: " + texto + "\r\n");
+                 }
+            	 if(j!=4) {
+            		 i++; 
+            	 }
+            	
+            }
+
+           
+            // También puedes manejar otros tipos de campos aquí (listas desplegables, etc.).
+
+            // ...
+        }
+
+       // try {
+            // Escribir los valores en un archivo .txt
+            //FileWriter writer = new FileWriter("ValoresDeTodosLosCampos.txt");
+        //writer.write(sb.toString());
+        // writer.flush();
+        // writer.close();
+        // } catch (IOException e) {
+        // e.printStackTrace();
+        //}
+
+        pdf.close();
+		
+		return null;
+		
+	}
+
+}
