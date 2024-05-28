8f5b0028dae59149b9847cf355c767658662cdec
Sergio T <166743935+SergioManTod@users.noreply.github.com>
2024-05-28 11:44:01 +0200

    Arreglo de clase BaseDeDatos

--- src/main/java/BaseDeDatos.java
+++ src/main/java/BaseDeDatos.java
@@ -1,15 +1,4 @@
 import java.io.FileInputStream;
-<<<<<<< HEAD
-	import java.io.FileNotFoundException;
-	import java.io.IOException;
-	import java.io.InputStream;
-	import java.sql.Connection;
-	import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-	import java.sql.SQLException;
-	import java.util.Properties;
-=======
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
@@ -18,9 +7,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
->>>>>>> branch 'main' of https://github.com/SergioManTod/GestorDeTorneos.git
 public class BaseDeDatos {
-<<<<<<< HEAD
+
 	Properties prop = new Properties(); 
 	InputStream is = null;
 	Connection con = null; 
@@ -45,12 +33,7 @@ public class BaseDeDatos {
 		String password = prop.getProperty("password", ""); 
 		String url = prop.getProperty("url", "");
 		String driver = prop.getProperty("driver", "");
-=======
-	private String txtConfiguracion;
-	Connection con = null;
->>>>>>> branch 'main' of https://github.com/SergioManTod/GestorDeTorneos.git
 
-<<<<<<< HEAD
 			try {
 				Class.forName(driver).newInstance();
 			} catch (InstantiationException e) {
@@ -100,79 +83,5 @@ public class BaseDeDatos {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
-	}
-=======
-	//CONSTRUCTORES
-	public BaseDeDatos(String txtConfiguracion) {
-		super();
-		this.txtConfiguracion = txtConfiguracion;
-	}
-
-	public BaseDeDatos() {
-		super();
-		// TODO Esbozo de constructor generado automáticamente
-	}
-	
-	// GETTERS Y SETTERS
-	public String getTxtConfiguracion() {
-		return txtConfiguracion;
-	}
-
-	public void setTxtConfiguracion(String txtConfiguracion) {
-		this.txtConfiguracion = txtConfiguracion;
-	}
-
-	public static Connection getCon() {
-		return con;
-	}
-
-	public static void setCon(Connection con) {
-		BaseDeDatos.con = con;
 	}	
-
-	//TOSTRING
-	@Override
-	public String toString() {
-		return "BaseDeDatos [txtConfiguracion=" + txtConfiguracion + "]";
-	}
-
-
-
-	//METODOS DE CONEXION
-	
-	public Connection conectarBd() {
-		
-		Properties prop = new Properties();
-		InputStream is = null;
-		try {
-			is = new FileInputStream("src/main/resources/bd.propertiedades_casa_sergio");
-			prop.load(is);
-			
-			String user = prop.getProperty("user","");
-			String password = prop.getProperty("password","");
-			String url = prop.getProperty("url","");
-			String driver = prop.getProperty("driver","");
-			
-			Class.forName(driver).newInstance();
-			con = DriverManager.getConnection(url, user, password);
-			
-		} catch (FileNotFoundException e) {
-			System.out.println("Archivo no encontrado");
-		} catch (InstantiationException e) {
-			System.out.println("No se ha podido instanciar");
-		} catch (IllegalAccessException e) {
-			System.out.println("Acceso no autorizado");
-		} catch (ClassNotFoundException e) {
-			System.out.println("Clase no encontrada");
-		} catch (IOException e) {
-			System.out.println("Error al conectarse a la base de datos.");
-		} catch (SQLException e) {
-			System.out.println("Error al conectarse a la base de datos.");
-		}
-		return con;
-	}
-	
-	
->>>>>>> branch 'main' of https://github.com/SergioManTod/GestorDeTorneos.git
-	
 }
